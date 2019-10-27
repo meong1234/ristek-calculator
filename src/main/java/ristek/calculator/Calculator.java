151421281831e78e5a6ea517796291c1ae4e389f
Bimo Horizon <bimozx@gmail.com>
2019-10-27 17:34:22 +0700

    Add calculator implementation

---
+++ src/main/java/ristek/calculator/Calculator.java
@@ -0,0 +1,16 @@
+package ristek.calculator;
+
+public class Calculator {
+  public int add(int x, int y) {
+    return x + y;
+  }
+  public int substract(int x, int y) {
+    return x - y;
+  }
+  public int multiply(int x, int y) {
+    return x * y;
+  }
+  public int divide(int x, int y) {
+    return x / y;
+  }
+}
