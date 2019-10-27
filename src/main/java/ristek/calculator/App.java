822e2ab7a14ca4c434b827180dbb73fc43098441
Bimo Horizon <bimozx@gmail.com>
2019-10-27 18:54:18 +0700

    Add app base implementation

--- src/main/java/ristek/calculator/App.java
+++ src/main/java/ristek/calculator/App.java
@@ -2,6 +2,31 @@ package ristek.calculator;
 
 public class App {
   public static void main(String[] args) {
-    System.out.print("1");
+    Calculator calculator = new Calculator();
+
+    String operation = args[0];
+    if (Calculator.isInvalidOperation(operation)) {
+      System.out.print("Operation input is invalid, expects " + Calculator.validOperations);
+      return;
+    }
+
+    int firstInput, secondInput;
+    firstInput = Integer.parseInt(args[1]);
+    secondInput = Integer.parseInt(args[2]);
+
+    switch (operation) {
+      case "add":
+        System.out.print(calculator.add(firstInput, secondInput));
+        break;
+      case "substract":
+        System.out.print(calculator.substract(firstInput, secondInput));
+        break;
+      case "multiply":
+        System.out.print(calculator.multiply(firstInput, secondInput));
+        break;
+      case "divide":
+        System.out.print(calculator.divide(firstInput, secondInput));
+        break;
+    }
   }
 }
