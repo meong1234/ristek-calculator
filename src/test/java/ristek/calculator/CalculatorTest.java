8c973e1a6fbadefe739cae26df5825e8257fe377
Bimo Horizon <bimozx@gmail.com>
2019-10-27 18:34:04 +0700

    Add test for calculator isValidOperation method

--- src/test/java/ristek/calculator/CalculatorTest.java
+++ src/test/java/ristek/calculator/CalculatorTest.java
@@ -2,7 +2,8 @@ package ristek.calculator;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Assertions;
 
 @DisplayName("Calculator Test")
 class CalculatorTest {
@@ -11,24 +12,40 @@ class CalculatorTest {
   @Test
   @DisplayName("Calculator Add Test")
   void addTest() {
-    assertEquals(2, calculator.add(1, 1));
+    Assertions.assertEquals(2, calculator.add(1, 1));
   }
 
   @Test
   @DisplayName("Calculator Substract Test")
   void substractTest() {
-    assertEquals(0, calculator.substract(1, 1));
+    Assertions.assertEquals(0, calculator.substract(1, 1));
   }
 
   @Test
   @DisplayName("Calculator Divide Test")
   void divideTest() {
-    assertEquals(1, calculator.divide(2, 2));
+    Assertions.assertEquals(1, calculator.divide(2, 2));
   }
 
   @Test
   @DisplayName("Calculator Multiply Test")
   void multiplyTest() {
-    assertEquals(4, calculator.multiply(2, 2));
+    Assertions.assertEquals(4, calculator.multiply(2, 2));
+  }
+
+  @Nested
+  @DisplayName("Calculator isValidOperation Tests")
+  class isValidOperationTests {
+    @Test
+    @DisplayName("Valid Test")
+    void validTest() {
+      Assertions.assertTrue(Calculator.isValidOperation("add"));
+    }
+
+    @Test
+    @DisplayName("Invalid Test")
+    void invalidTest() {
+      Assertions.assertFalse(Calculator.isValidOperation("foo"));
+    }
   }
 }
